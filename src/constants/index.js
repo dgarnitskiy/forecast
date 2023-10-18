eb5d1ecb64bc4346485cf657067b06f285bfd331
dgarnitskiy <danya.garnitskiy@gmail.com>
2023-10-18 16:11:00 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+
+export const API_KEY = import.meta.env.VITE_AUTH_TOKEN
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+const PRESSURE_UNITS = 0.750062
+
+export const capitalizeFirstLetter = (str) => str ? str.charAt(0).toUpperCase() + str.slice(1) : ''
+
+export const getPressureMm = (hpa) => {
+	return Math.round(hpa * PRESSURE_UNITS)
+}
+
+export const getTime = (seconds) => {
+	return new Date(seconds * 1000).toLocaleTimeString('ru-Ru', {timeZone: 'Atlantic/Reykjavik'})
+}
